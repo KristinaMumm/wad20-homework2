da7112e7d9bc09b6383d410d49784f6aaba6e60f
Henri Sellis <henri.sellis@gmail.com>
2020-10-22 18:55:50 +0300

    basic functionality of browse page done

--- res/js/browse-script.js
+++ res/js/browse-script.js
@@ -1,19 +1,24 @@
+let profiles = [];
+
 $(function () {
-    
+
     loadProfilesInfo()
         .then( function (response) {
-            for (profile in response){
-                let profile = new Profile(
+            for (let profile of response){
+                profiles.push(new Profile (
                     profile.firstname,
                     profile.lastname,
                     profile.avatar
-                );
+                    )
+                )
             }
+
+            displayProfiles();
         })
         .catch(function () {
             alert('Error loading profiles')
         });
-};
+});
 
 function loadProfilesInfo() {
     return $.get(
@@ -27,4 +32,19 @@ function loadProfilesInfo() {
             }
         }
     )
+}
+
+function displayProfiles() {
+    for (let profile of profiles){
+        let profileDiv = $('div')
+        let avatar = $('<img>').attr('src', profile.avatar)
+        let name = $('<p>').text(profile.firstName + ' ' + profile.lastName)
+        let followButton = $('<button>').text('Follow')
+
+        profileDiv.append(avatar)
+        profileDiv.append(name)
+        profileDiv.append(followButton)
+
+        $('section#main-container tbody').append(profileDiv)
+    }
 }
