4b3956e4c326c24b52de4169dae167b61de2a407
Henri Sellis <henri.sellis@gmail.com>
2020-10-22 21:02:16 +0300

    Removed unnecessary duplicated browse functions from main script

--- res/js/script.js
+++ res/js/script.js
@@ -46,32 +46,3 @@ function displayUserInfo(user) {
     $('#dropdown-content #name').text(user.firstname + ' ' + user.lastname);
     $("#dropdown-content #email").text(user.email);
 }
-
-function loadProfilesInfo() {
-    return $.get(
-        {
-            url: "https://private-anon-b63c1d7892-wad20postit.apiary-mock.com/profiles",
-            success: function (response) {
-                return response ;
-            },
-            error: function() {
-                alert("Error with reading profiles")
-            }
-        }
-    )
-}
-
-function displayProfiles() {
-    for (let profile of profiles){
-        let profileDiv = $('div')
-        let avatar = $('<img>').attr('src', profile.avatar)
-        let name = $('<p>').text(profile.firstName + ' ' + profile.lastName)
-        let followButton = $('<button>').text('Follow')
-
-        profileDiv.append(avatar)
-        profileDiv.append(name)
-        profileDiv.append(followButton)
-
-        $('section#main-container tbody').append(profileDiv)
-    }
-}
